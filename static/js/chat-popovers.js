695383284485ec15ab59c9e4fc120a99fef73bdc
Nicholas Carey <ncarey2400@gmail.com>
2023-11-14 13:09:34 -0500

    Added the Question Daemon V1

--- static/js/chat-popovers.js
+++ static/js/chat-popovers.js
@@ -1,12 +1,6 @@
 // Function to generate chatbox HTML content
 function getChatboxContent() {
-    var defaultLioMessage = `
-    <div class="d-flex flex-row justify-content-start">
-      <div>
-        <p class="small p-2 ms-3 mb-3 rounded-3" style="background-color: #f5f6f7;">Hi, I'm happy to help!</p>
-      </div>
-    </div>
-  `;
+
     return `
       <div class="chat-interface">
           <div class="card">
@@ -14,7 +8,6 @@ function getChatboxContent() {
                   <h5 class="mb-0">Ask Lio a Question</h5>
               </div>
               <div class="card-body chat-messages" style="height: 200px; overflow-y: auto;">
-              ${defaultLioMessage} 
               </div>
               <div class="card-footer text-muted p-3">
                   <div class="input-group">
@@ -45,6 +38,13 @@ function reinitializePopovers() {
       } else {
         content = "Coming Soon!"; // Text for feedback and encouragement daemons
       }
+
+      if (popoverTriggerEl.id ==='question-daemon'){
+        popoverTriggerEl.addEventListener('shown.bs.popover', function(){
+            loadChatMessages();
+        })
+      }
+
   
       // Initialize a new popover
       new bootstrap.Popover(popoverTriggerEl, {
@@ -54,19 +54,23 @@ function reinitializePopovers() {
         sanitize: false,
         content: content
       });
+      
     });
 }
 
 // Updated function to append messages to chat body inside popover
 function appendMessageToChat(container, message, isUser) {
+    var htmlMessage = marked.parse(message); // Convert Markdown to HTML
+    var safeHtmlMessage = DOMPurify.sanitize(htmlMessage); // Sanitize the HTML content
+
     var messageBubble = document.createElement('div');
-    messageBubble.classList.add('d-flex', isUser ? 'justify-content-end' : 'justify-content-start');
+    messageBubble.classList.add('d-flex', isUser ? 'justify-content-end' : 'justify-content-start', 'chat-message');
 
     var messageContent = document.createElement('p');
     messageContent.classList.add('small', 'p-2', 'mb-3', 'rounded-3');
     // Set the background color based on whether the message is from the user or the assistant
-    messageContent.style.backgroundColor = isUser ? '#dcf8c6' : '#f5f6f7'; // Choose colors as per your design
-    messageContent.innerHTML = DOMPurify.sanitize(message);
+    messageContent.style.backgroundColor = isUser ? '#dcf8c6' : '#f5f6f7';
+    messageContent.innerHTML = safeHtmlMessage; // Use the sanitized HTML content
 
     var wrapperDiv = document.createElement('div'); // Extra wrapper for additional styling if needed
     wrapperDiv.appendChild(messageContent);
@@ -76,6 +80,23 @@ function appendMessageToChat(container, message, isUser) {
     container.scrollTop = container.scrollHeight; // Auto scroll to latest message
 }
 
+function loadChatMessages() {
+    var chatContainer = document.querySelector('.chat-interface .chat-messages');
+    if (chatContainer) {
+        fetch('/get_messages')
+        .then(response => response.json())
+        .then(data => {
+            chatContainer.innerHTML = ''; // Clear previous messages
+            data.forEach(message => {
+                appendMessageToChat(chatContainer, message.content, message.role === 'user');
+            });
+            chatContainer.scrollTop = chatContainer.scrollHeight;
+        })
+        .catch(error => {
+            console.error('Error loading messages:', error);
+        });
+    }
+}
 
 // Call reinitializePopovers on DOMContentLoaded or when you need to set up the popovers
 document.addEventListener('DOMContentLoaded', function () {
@@ -85,6 +106,8 @@ document.addEventListener('DOMContentLoaded', function () {
 document.body.addEventListener('click', function (event) {
     if (event.target.matches('.send-chat-message')) {
         var chatInput = event.target.closest('.chat-interface').querySelector('.chat-input');
+        const projectTemplateId = document.getElementById('project-template-id').value; //Project Template ID
+
         var message = chatInput.value.trim();
         if (message) {
             // Sanitize the message
@@ -98,7 +121,8 @@ document.body.addEventListener('click', function (event) {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
-                    user_message: sanitizedMessage
+                    user_message: sanitizedMessage,
+                    project_template_id: projectTemplateId
                 }),
             })
             .then(response => response.json())
@@ -111,7 +135,7 @@ document.body.addEventListener('click', function (event) {
                     appendMessageToChat(chatInput.closest('.chat-interface').querySelector('.chat-messages'), data.message, false);
                 } else {
                     // Handle the case where there's an error in response
-                    console.error('Error from the assistant:', data.error);
+                    console.error('Lio is currently down, please try again later:', data.error);
                 }
             })
 
